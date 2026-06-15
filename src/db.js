82a406f876b20012f80ba7ead36e0be077db1070
kpcastillo <krn_iveth09@hotmail.com>
2026-06-14 20:21:08 -0700

    db connection function edited

--- src/db.js
+++ src/db.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 async function init() {
   await pool.query(`
